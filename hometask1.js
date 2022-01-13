82f6a05a3c569b8652d73a77102fb153a293ae7b
ddudarova <56075892+ddudarova@users.noreply.github.com>
2022-01-13 16:26:01 +0200

    Add files via upload

--- hometask1.js
+++ hometask1.js
@@ -1,29 +1,29 @@
-/*//hometask point 2
-console.log('Dudarova');*/
+//hometask point 2
+console.log('Dudarova');
 
 //hometask point 3
-/*let first = 'first_variable';
+let first = 'first_variable';
 let second = 'second_variable';
 document.write(first, second)
 first = second;
-document.write(first)*/
+document.write(first)
 
-//hometask point 4 (question about undefined)
-/*let dataTypesObject = {
-    number:55,
-    string:'Ivan',
-    boolean:true,
-    null:null,
-    undefined:undefined
+//hometask point 4
+let dataTypesObject = {
+    number: 55,
+    string: 'Ivan',
+    boolean: true,
+    null: null,
+    undefined: undefined
 }
-console.log(dataTypesObject)*/
+console.log(dataTypesObject)
 
 //hometask point 5
-/*let isAdult = confirm('Are you an adult?');
-console.log(isAdult)*/
+let isAdult = confirm('Are you an adult?');
+console.log(isAdult)
 
 //hometask point 6
-/*const myName = 'Daria';
+const myName = 'Daria';
 const mySurname = 'Dudarova';
 const myGroup = '1A';
 const myBirthYear = 1995;
@@ -31,16 +31,16 @@ const isMarried = true;
 let myMiddleName;
 let myAde = null;
 
-console.log(typeof myBirthYear,typeof isMarried,typeof myName,typeof mySurname,typeof myGroup, typeof myMiddleName, typeof myAde)*/
+console.log(typeof myBirthYear, typeof isMarried, typeof myName, typeof mySurname, typeof myGroup, typeof myMiddleName, typeof myAde)
 
 //hometask point 7
-/*let login = prompt('Login', 'Ivan123');
+let login = prompt('Login', 'Ivan123');
 alert(login);
 let email = prompt('Email', 'Ivan123@gmail.com');
 alert(email);
 let password = prompt('Password', 'qwerty12345');
 alert(password);
-alert('Dear ' + login + ', your email is ' + email + ', your password is ' + password)*/
+alert('Dear ' + login + ', your email is ' + email + ', your password is ' + password)
 
 //hometask point 8
 let sec = 1;
