2043f0d82ed8c7e4cd8d822a558465708b613058
ddudarova <56075892+ddudarova@users.noreply.github.com>
2022-01-13 16:21:14 +0200

    Add files via upload

---
+++ hometask1.js
@@ -0,0 +1,53 @@
+/*//hometask point 2
+console.log('Dudarova');*/
+
+//hometask point 3
+/*let first = 'first_variable';
+let second = 'second_variable';
+document.write(first, second)
+first = second;
+document.write(first)*/
+
+//hometask point 4 (question about undefined)
+/*let dataTypesObject = {
+    number:55,
+    string:'Ivan',
+    boolean:true,
+    null:null,
+    undefined:undefined
+}
+console.log(dataTypesObject)*/
+
+//hometask point 5
+/*let isAdult = confirm('Are you an adult?');
+console.log(isAdult)*/
+
+//hometask point 6
+/*const myName = 'Daria';
+const mySurname = 'Dudarova';
+const myGroup = '1A';
+const myBirthYear = 1995;
+const isMarried = true;
+let myMiddleName;
+let myAde = null;
+
+console.log(typeof myBirthYear,typeof isMarried,typeof myName,typeof mySurname,typeof myGroup, typeof myMiddleName, typeof myAde)*/
+
+//hometask point 7
+/*let login = prompt('Login', 'Ivan123');
+alert(login);
+let email = prompt('Email', 'Ivan123@gmail.com');
+alert(email);
+let password = prompt('Password', 'qwerty12345');
+alert(password);
+alert('Dear ' + login + ', your email is ' + email + ', your password is ' + password)*/
+
+//hometask point 8
+let sec = 1;
+let min = sec * 60;
+let hour = min * 60;
+let day = hour * 24;
+let month = day * 30;
+document.write('There is ', hour, ' seconds in 1 hour, ', day, ' seconds in 1 day, ', 'and ', month, ' seconds in 1 30-days month')
+
+//console.log(hour, day, months)
