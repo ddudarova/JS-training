a36573bc055a20be9283d74d3217076dfa7a1c66
ddudarova <dashkadudarova@gmail.com>
2022-01-14 15:35:00 +0200

    adding to homework2 task point 2

--- hometask2.js
+++ hometask2.js
@@ -1,19 +1,28 @@
 //point 1
-let x = 1;
-let y = 2;
+// let x = 1;
+// let y = 2;
+//
+// let res1 = x.toString() + y;
+// console.log(res1);
+// console.log(typeof res1);
+//
+// let res2 = !!x + y.toString();
+// console.log(res2);
+// console.log(typeof res2);
+//
+// let res3 = !!(x + y);
+//     console.log(res3);
+// console.log(typeof res3);
+//
+// let res4 = 'a' * x * y;
+// console.log(res4);
+// console.log(typeof res4);
 
-let res1 = x.toString() + y;
-console.log(res1);
-console.log(typeof res1);
+//point2
+const number = prompt('Please enter number', '');
+if (number > 0 && number % 2 === 0 && number % 7 === 0) {
+    console.log('Number ' + number + ' is valid');
+} else {
+    console.log('Number ' + number + ' is NOT valid')
+}
 
-let res2 = !!x + y.toString();
-console.log(res2);
-console.log(typeof res2);
-
-let res3 = !!(x + y);
-    console.log(res3);
-console.log(typeof res3);
-
-let res4 = 'a' * x * y;
-console.log(res4);
-console.log(typeof res4);
