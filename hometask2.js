52bcf5e85a5a3cbd13124cdaa599fc2bd749f468
ddudarova <dashkadudarova@gmail.com>
2022-01-17 20:51:12 +0200

    adding to homework2 task point 6

--- hometask2.js
+++ hometask2.js
@@ -19,10 +19,55 @@
 // console.log(typeof res4);
 
 //point2
-const number = prompt('Please enter number', '');
-if (number > 0 && number % 2 === 0 && number % 7 === 0) {
-    console.log('Number ' + number + ' is valid');
+// const number = prompt('Please enter number', '');
+// if (number > 0 && number % 2 === 0 && number % 7 === 0) {
+//     console.log('Number ' + number + ' is valid');
+// } else {
+//     console.log('Number ' + number + ' is NOT valid')
+// }
+
+//point3
+// const newArray = [];
+// newArray.push(2,'test',true,null);
+// document.write(newArray.length);
+// const userNum = prompt('Please enter number', '');
+// newArray.push(userNum);
+// document.write(userNum);
+// newArray.shift();
+// document.write(newArray);
+
+//point4
+// let cities = ["Rome", "Lviv", "Warsaw"];
+// let str = cities.join('*');
+// console.log(str);
+
+//point5
+// const isAdult = prompt('Please enter your age', '');
+// if (isAdult > 0 && isAdult < 18) {
+//     alert('You are too young');
+// } else if (isAdult >= 18) {
+//     alert('You have reached the age of majority');
+// }
+// else {
+//     alert('Invalid number');
+// }
+
+//point6
+const A = Number(prompt('Please enter length of A side', ''));
+const B = Number(prompt('Please enter length of B side', ''));
+const C = Number(prompt('Please enter length of C side', ''));
+
+if ((isNaN(A) || isNaN(B) || isNaN(C)) || (A < 1 || B < 1 || C < 1)) {
+    console.log('Incorrect Data');
 } else {
-    console.log('Number ' + number + ' is NOT valid')
+    const P = (A + B + C) / 2;
+    const S = (P * (P - A) * (P - B) * (P - C)) ** (1 / 2);
+    console.log(S.toFixed(2))
+    const isRA = A ** 2 + B ** 2 === C ** 2;
+    if (isRA === true) {
+        console.log('This  triangle is right angled');
+    } else {
+        console.log('This  triangle is not right angled');
+    }
 }
 
