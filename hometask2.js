215b506ff0edb1e0f2d76bbb63e63da0277849e5
ddudarova <dashkadudarova@gmail.com>
2022-01-18 16:49:47 +0200

    hometask last part

--- hometask2.js
+++ hometask2.js
@@ -1,56 +1,56 @@
 //point 1
-// let x = 1;
-// let y = 2;
-//
-// let res1 = x.toString() + y;
-// console.log(res1);
-// console.log(typeof res1);
-//
-// let res2 = !!x + y.toString();
-// console.log(res2);
-// console.log(typeof res2);
-//
-// let res3 = !!(x + y);
-//     console.log(res3);
-// console.log(typeof res3);
-//
-// let res4 = 'a' * x * y;
-// console.log(res4);
-// console.log(typeof res4);
+let x = 1;
+let y = 2;
+
+let res1 = x.toString() + y;
+console.log(res1);
+console.log(typeof res1);
+
+let res2 = !!x + y.toString();
+console.log(res2);
+console.log(typeof res2);
+
+let res3 = !!(x + y);
+    console.log(res3);
+console.log(typeof res3);
+
+let res4 = 'a' * x * y;
+console.log(res4);
+console.log(typeof res4);
 
 //point2
-// const number = prompt('Please enter number', '');
-// if (number > 0 && number % 2 === 0 && number % 7 === 0) {
-//     console.log('Number ' + number + ' is valid');
-// } else {
-//     console.log('Number ' + number + ' is NOT valid')
-// }
+const number = prompt('Please enter number', '');
+if (number > 0 && number % 2 === 0 && number % 7 === 0) {
+    console.log('Number ' + number + ' is valid');
+} else {
+    console.log('Number ' + number + ' is NOT valid')
+}
 
 //point3
-// const newArray = [];
-// newArray.push(2,'test',true,null);
-// document.write(newArray.length);
-// const userNum = prompt('Please enter number', '');
-// newArray.push(userNum);
-// document.write(userNum);
-// newArray.shift();
-// document.write(newArray);
+const newArray = [];
+newArray.push(2,'test',true,null);
+document.write(newArray.length);
+const userNum = prompt('Please enter number', '');
+newArray.push(userNum);
+document.write(userNum);
+newArray.shift();
+document.write(newArray);
 
 //point4
-// let cities = ["Rome", "Lviv", "Warsaw"];
-// let str = cities.join('*');
-// console.log(str);
+let cities = ["Rome", "Lviv", "Warsaw"];
+let str = cities.join('*');
+console.log(str);
 
 //point5
-// const isAdult = prompt('Please enter your age', '');
-// if (isAdult > 0 && isAdult < 18) {
-//     alert('You are too young');
-// } else if (isAdult >= 18) {
-//     alert('You have reached the age of majority');
-// }
-// else {
-//     alert('Invalid number');
-// }
+const isAdult = prompt('Please enter your age', '');
+if (isAdult > 0 && isAdult < 18) {
+    alert('You are too young');
+} else if (isAdult >= 18) {
+    alert('You have reached the age of majority');
+}
+else {
+    alert('Invalid number');
+}
 
 //point6
 const A = Number(prompt('Please enter length of A side', ''));
@@ -71,3 +71,37 @@ if ((isNaN(A) || isNaN(B) || isNaN(C)) || (A < 1 || B < 1 || C < 1)) {
     }
 }
 
+//point7
+const curTime = (new Date()).getHours();
+console.log(curTime);
+
+if (curTime === 23 || curTime >= 0 && curTime < 5) {
+    console.log('Good night');
+} else if (curTime >= 5 && curTime < 11) {
+    console.log('Good morning');
+} else if (curTime >= 11 && curTime < 17) {
+    console.log('Good afternoon');
+} else if (curTime >= 17 && curTime < 23) {
+    console.log('Good evening');
+} else {
+    console.log('Incorrect data');
+}
+
+switch (true) {
+    case curTime === 23:
+        console.log('Good night');
+        break
+    case curTime >= 0 && curTime < 5:
+        console.log('Good night');
+        break
+    case curTime >=5 && curTime < 11:
+        console.log('Good morning');
+        break
+    case curTime >= 11 && curTime < 17:
+        console.log('Good afternoon');
+        break
+    case curTime >= 17 && curTime < 23:
+        console.log('Good evening');
+        break
+
+}
