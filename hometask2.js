b14717be4e19b0fb527488a000ddf14324dd34f1
ddudarova <dashkadudarova@gmail.com>
2022-01-20 22:42:12 +0200

    hometask 3

--- hometask2.js
+++ hometask2.js
@@ -1,75 +1,75 @@
-//point 1
-let x = 1;
-let y = 2;
-
-let res1 = x.toString() + y;
-console.log(res1);
-console.log(typeof res1);
-
-let res2 = !!x + y.toString();
-console.log(res2);
-console.log(typeof res2);
-
-let res3 = !!(x + y);
-    console.log(res3);
-console.log(typeof res3);
-
-let res4 = 'a' * x * y;
-console.log(res4);
-console.log(typeof res4);
-
-//point2
-const number = prompt('Please enter number', '');
-if (number > 0 && number % 2 === 0 && number % 7 === 0) {
-    console.log('Number ' + number + ' is valid');
-} else {
-    console.log('Number ' + number + ' is NOT valid')
-}
-
-//point3
-const newArray = [];
-newArray.push(2,'test',true,null);
-document.write(newArray.length);
-const userNum = prompt('Please enter number', '');
-newArray.push(userNum);
-document.write(userNum);
-newArray.shift();
-document.write(newArray);
-
-//point4
-let cities = ["Rome", "Lviv", "Warsaw"];
-let str = cities.join('*');
-console.log(str);
-
-//point5
-const isAdult = prompt('Please enter your age', '');
-if (isAdult > 0 && isAdult < 18) {
-    alert('You are too young');
-} else if (isAdult >= 18) {
-    alert('You have reached the age of majority');
-}
-else {
-    alert('Invalid number');
-}
-
-//point6
-const A = Number(prompt('Please enter length of A side', ''));
-const B = Number(prompt('Please enter length of B side', ''));
-const C = Number(prompt('Please enter length of C side', ''));
-
-if ((isNaN(A) || isNaN(B) || isNaN(C)) || (A < 1 || B < 1 || C < 1)) {
-    console.log('Incorrect Data');
-} else {
-    const P = (A + B + C) / 2;
-    const S = (P * (P - A) * (P - B) * (P - C)) ** (1 / 2);
-    console.log(S.toFixed(2))
-    const isRA = A ** 2 + B ** 2 === C ** 2;
-    if (isRA === true) {
-        console.log('This  triangle is right angled');
-    } else {
-        console.log('This  triangle is not right angled');
-    }
-}
+// //point 1
+// let x = 1;
+// let y = 2;
+//
+// let res1 = x.toString() + y;
+// console.log(res1);
+// console.log(typeof res1);
+//
+// let res2 = !!x + y.toString();
+// console.log(res2);
+// console.log(typeof res2);
+//
+// let res3 = !!(x + y);
+//     console.log(res3);
+// console.log(typeof res3);
+//
+// let res4 = 'a' * x * y;
+// console.log(res4);
+// console.log(typeof res4);
+//
+// //point2
+// const number = prompt('Please enter number', '');
+// if (number > 0 && number % 2 === 0 && number % 7 === 0) {
+//     console.log('Number ' + number + ' is valid');
+// } else {
+//     console.log('Number ' + number + ' is NOT valid')
+// }
+//
+// //point3
+// const newArray = [];
+// newArray.push(2,'test',true,null);
+// document.write(newArray.length);
+// const userNum = prompt('Please enter number', '');
+// newArray.push(userNum);
+// document.write(userNum);
+// newArray.shift();
+// document.write(newArray);
+//
+// //point4
+// let cities = ["Rome", "Lviv", "Warsaw"];
+// let str = cities.join('*');
+// console.log(str);
+//
+// //point5
+// const isAdult = prompt('Please enter your age', '');
+// if (isAdult > 0 && isAdult < 18) {
+//     alert('You are too young');
+// } else if (isAdult >= 18) {
+//     alert('You have reached the age of majority');
+// }
+// else {
+//     alert('Invalid number');
+// }
+//
+// //point6
+// const A = Number(prompt('Please enter length of A side', ''));
+// const B = Number(prompt('Please enter length of B side', ''));
+// const C = Number(prompt('Please enter length of C side', ''));
+//
+// if ((isNaN(A) || isNaN(B) || isNaN(C)) || (A < 1 || B < 1 || C < 1)) {
+//     console.log('Incorrect Data');
+// } else {
+//     const P = (A + B + C) / 2;
+//     const S = (P * (P - A) * (P - B) * (P - C)) ** (1 / 2);
+//     console.log(S.toFixed(2))
+//     const isRA = A ** 2 + B ** 2 === C ** 2;
+//     if (isRA === true) {
+//         console.log('This  triangle is right angled');
+//     } else {
+//         console.log('This  triangle is not right angled');
+//     }
+// }
 
 //point7
 const curTime = (new Date()).getHours();
---
+++ hometask3.js
@@ -0,0 +1,71 @@
+//point1
+const numbers = [2,3,4,5];
+
+let result = 1;
+for (let i = 0; i < numbers.length; i++) {
+    result *= numbers[i];
+}
+console.log(result);
+
+let i = 0;
+while (i < numbers.length) {
+    result *= numbers[i];
+    i++;
+}
+console.log(result);
+
+//point2
+for(let count = 0; count < 16; count++) {
+    if (count % 2 === 0) {
+        console.log(count + ' is even');
+    } else {
+        console.log(count + ' is odd');
+    }
+}
+
+//point3
+function getRandomInt(a, b) {
+    let k = [];
+    for (let i=a, t=b; i<t; i++) {
+        k.push(Math.round(Math.random() * t))
+    }
+    console.log(k);
+}
+getRandomInt(1,500)
+
+//point4
+function findMin() {
+    const showMin = Math.min(...arguments);
+    console.log(showMin);
+}
+findMin(12, 14, 4, -4, 0.2, -12);
+
+//point5
+function findUnique(arr) {
+    const result = [];
+    for (let i of arr) {
+        if (!result.includes(i)) {
+            result.push(i);
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(findUnique([1, 2, 3, 4]));
+
+// point6
+function lastElem(a,b) {
+    let c = a.slice(-b);
+    console.log(c);
+}
+lastElem([3, 4, 10, -5],2);
+
+// point7
+function ucFirst(str) {
+    const newStr = str[0].toUpperCase() + str.slice(1);
+    console.log(newStr);
+}
+ucFirst('this is string');
