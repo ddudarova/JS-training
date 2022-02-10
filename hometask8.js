0fc6addb64e266bf782dd578e13b6c084bf9aa51
ddudarova <dashkadudarova@gmail.com>
2022-02-10 10:31:24 +0200

    Add hometask7 file

--- hometask8.js
+++ hometask8.js
@@ -1,50 +1,50 @@
 //point1
-// function upperCase(str) {
-//     const regExp = /^[A-Z]/.test(str);
-//     if (regExp) {
-//         console.log('String starts with uppercase character ');
-//     } else {
-//         console.log('String is not start with uppercase character ');
-//     }
-// }
-// upperCase('regexp');
-// upperCase('RegExp');
+function upperCase(str) {
+    const regExp = /^[A-Z]/.test(str);
+    if (regExp) {
+        console.log('String starts with uppercase character ');
+    } else {
+        console.log('String is not start with uppercase character ');
+    }
+}
+upperCase('regexp');
+upperCase('RegExp');
 
 //point2
-// function checkEmail(str){
-//     const regExp = /^[\.\w\-]+@[a-z0-9\-\.]+\.com$/i;
-//     console.log(regExp.test(str));
-// }
-//
-// checkEmail("Qmail2@gmail...com");
+function checkEmail(str){
+    const regExp = /^[\.\w\-]+@[a-z0-9\-\.]+\.com$/i;
+    console.log(regExp.test(str));
+}
+
+checkEmail("Qmail2@gmail...com");
 
 
 //point5
-// const regExp1 = /\w+/g;
-// const newAr = "Java Script".match(regExp1);
-// const newStr = "Java Script".replace(/^\w+/, newAr[1]);
-// console.log(newStr.replace(/\w+$/, newAr[0]));
+const regExp1 = /\w+/g;
+const newAr = "Java Script".match(regExp1);
+const newStr = "Java Script".replace(/^\w+/, newAr[1]);
+console.log(newStr.replace(/\w+$/, newAr[0]));
 
 //point6
-// function checkEmail(str){
-//     const regExp = /^[^\-]\w+[\-]{0,1}\w+@[a-z0-9]+\.com$/i;
-//     const res =  regExp.test(str);
-//     if (res){
-//         console.log('Email is correct!');
-//     } else {
-//         console.log('Email is not correct!');
-//     }
-// }
-//
-// checkEmail("Qmail2@gmail.com");
+function checkEmail(str){
+    const regExp = /^[^\-]\w+[\-]{0,1}\w+@[a-z0-9]+\.com$/i;
+    const res =  regExp.test(str);
+    if (res){
+        console.log('Email is correct!');
+    } else {
+        console.log('Email is not correct!');
+    }
+}
+
+checkEmail("Qmail2@gmail.com");
 
 //point7
-// function checkLogin(login){
-//     const checkExp = /^[^\d]([a-z0-9]*[.]?[0-9]+){2,10}$/i;
-//     // const checkExp = /^[^\d](?:[a-z0-9]|(:?\d+\.?\d+)){2,10}$/i;
-//     const printNum = /[0-9]*[.]?[0-9]+/g;
-//     console.log(checkExp.test(login));
-//     console.log(login.match(printNum).join(', '));
-//
-// }
-// checkLogin('ee1*1ret3');
+function checkLogin(login){
+    const checkExp = /^[^\d]([a-z0-9]*[.]?[0-9]+){2,10}$/i;
+    const printNum = /[0-9]*[.]?[0-9]+/g;
+    console.log(checkExp.test(login));
+    console.log(login.match(printNum).join(', '));
+
+}
+checkLogin('ee1.1ret3');
+checkLogin('ee1*1ret3');
